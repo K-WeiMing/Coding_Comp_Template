02b19d4a6a933e06d5cbcc394c5148908e59ac1a
Kng Wei Ming <65479050+K-WeiMing@users.noreply.github.com>
2022-03-21 17:54:23 +0800

    Update coding_comp.py

--- coding_comp.py
+++ coding_comp.py
@@ -1,17 +1,18 @@
 # Templates to be used for python in coding competitions such as Google KickStart
     
 # Single line input
-    
 t = int(input())
 for _ in range(t):
     a, b, bound = list(map(int, input().split()))
-
     # Execute code here
     
-    
+
 # Two or more line inputs
 t = int(input())
 
 for t in range(int(input())):
     i = [x for x in input()]
     p = [x for x in input()]
+    # Add inputs as necessary
+    
+    # Execute code here
